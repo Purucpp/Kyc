7b5f9e3834b4e17eb9275d25693743907c098932
purushottam kumar <purushottam.kumar@janitri.in>
2022-04-17 18:41:09 +0530

    temp

--- app/src/main/java/com/yesandroid/kyc/MainActivity.kt
+++ app/src/main/java/com/yesandroid/kyc/MainActivity.kt
@@ -2,10 +2,21 @@ package com.yesandroid.kyc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.next)
+
+        button.setOnClickListener{
+            Toast.makeText(applicationContext, "Test", Toast.LENGTH_LONG).show()
+        }
     }
 }
